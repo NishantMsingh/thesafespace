47a507d9c85c02b93091073e76be90cee9b14bde
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-03 16:44:16 +0530

    working on hero

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -7,18 +7,7 @@ const HeroSection = () => {
  
   return (
     <section id='hero' className={classes["hero-section"]}>
-        <div className={classes["hero-text-container"]}>
-          <div className={classes["flex-center"]}>
-          <span className=" ">HELLO </span>
-          <span className=" "> Web  Designer & Front-end Developer</span>
-          <span className=" "> Passionate about changing the world with technology.
-</span>
-          <div className={classes.links}>
-             <span className={classes["links-hero"]}> <a target='blank' href='https://www.linkedin.com/in/nishant-kumar-124595284/'><BiLogoLinkedin fontSize={"1.5rem"}/></a></span>
-             <span className={classes["links-hero"]}> <a target='blank' href='https://www.facebook.com/profile.php?id=100095054529905'><FaFacebookF fontSize={"1.5rem"}/></a></span>
-          </div>
-          </div>
-        </div>
+       
     </section>
   );
 };
