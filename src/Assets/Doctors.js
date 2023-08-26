0d6d18f59a445b41c38b19db345b51a6f5c44209
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-26 15:17:45 +0530

    addding docs

--- src/Assets/Doctors.js
+++ src/Assets/Doctors.js
@@ -1,24 +1,44 @@
 import priyankimg from "./Images/PriyankqRawlani.png";
+import roshniimg from "./Images/RaushniKhanna.png";
 
 
-const Doctors=[
-{
-    idphone:"9702712979",
-    pronoun:"She/her",
-    name:"Priyanka Rawlani",
-    email:"priyankarawlani18@gmail.com",
-    phone:"9702712979",
-    description:"Expressive Arts Therapy Practitioner",
-    location:"Delhi",
-    experience:"1 Year",
-    price:"1500 (with a sliding scale)",
-    pic:priyankimg,
-    brief:`Hello! I’m Priyanka , an expressive arts therapy practitioner and an artist! 
+const Doctors = [
+    {
+        idphone: "9702712979",
+        pronoun: "She/her",
+        name: "Priyanka Rawlani",
+        email: "priyankarawlani18@gmail.com",
+        phone: "9702712979",
+        description: `MSc in Psychology (HRDM) - Christ University, Bangalore  \n
+    PG Diploma in Integrated Counselling - Nibbana accredited by Madras School of Social Work \n
+    Certification in Trauma Informed Care - TISS Mumbai `,
+        location: "Delhi",
+        experience: "1 Year",
+        price: "1500 (with a sliding scale)",
+        pic: priyankimg,
+
+        brief: `Hello! I’m Priyanka , an expressive arts therapy practitioner and an artist! 
     I use the client centered approach in my therapy , where I guide the clients into taking the creative tools of expressive arts as a medium of expression and channelisation for their emotions. 
     Expressing and relieving one’s feelings can lead to wellness and healing  - overall. And sometimes a lot of trauma, grief, emotions can get stuck in our bodies and our subconscious. 
     The creative arts can become a guiding force for this relief and awareness! `,
-    specification:`I am Queer affirmative and Trauma informed in my training as well. 
+        specification: `I am Queer affirmative and Trauma informed in my training as well. 
     I have also completed my training in Music in wellness from Berklee college of music. `
-}
+    }
+    ,
+    {
+        idphone: "9702712979",
+        pronoun: "She/her",
+        name: "Raushni Khanna",
+        email: "raushnikhanna@gmail.com",
+        phone: "9702712979",
+        description: "Expressive Arts Therapy Practitioner",
+        location: "Delhi",
+        experience: "2.8 Years",
+        price: "2000 for Indian Clients & 4000 for Indian clients living abroad or clients from other nationalities",
+        pic: roshniimg,
+        brief: `I’m a Trauma Informed Therapist and I use an eclectic and strengths-based approach.`,
+        specification: `I mostly work with clients between the age of 18-40. years. I have helped clients across various geographies (India, Australia, Germany, UK, Canada, New Zealand & USA) with issues related to Relationships, Work related Stress, Career Guidance, Marital & Family issues, Infidelity, Divorce, Sexual Intimacy, Boundaries, Low Self-Esteem, Eating Disorders, Body Image, Sex & Sexuality, Financial Management, etc. I also conduct various mental health awareness sessions for Corporate Employees and College Students. Some of the organizations & institute that I have worked with include, Cisco, Sify, Social Beat, iLink Digital, etc. & M.O.P.Vaishnav College, Jai Hind College, Bharti College, DPS, etc. \n I am passionate about creating content that encourages people to be vulnerable, self-reflective and self-accepting of themselves through my Instagram page - @thesubtleartofexpression`
+    }
+
 ]
-export  default Doctors;
+export default Doctors;
