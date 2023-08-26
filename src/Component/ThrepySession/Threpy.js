8e89473aecbeb64148cec1b4d7e56b173868b303
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-26 14:28:37 +0530

    doneee

--- src/Component/ThrepySession/Threpy.js
+++ src/Component/ThrepySession/Threpy.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import "./Threpy.css"
-import { Link } from 'react-router-dom';
+
 import ThrepistList from './ThrepistList';
 
 const Threpy = () => {
   return (
-    
+    <div className='container-fluid'>
+    <div className="d-flex align-items-center justify-content-center p-5"> <span className=' brand shadow rounded pt-1 pb-1 ps-3 pe-3 select fs-3'> Book the threpy session with us </span>  </div>
+    <ThrepistList/>
+    </div>
 
 
-<ThrepistList/>
+
   
   )
 }
