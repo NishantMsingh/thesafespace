0c9170d9938b057640fc5d61e15df03a9aa6890d
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-26 15:01:11 +0530

    odsos

--- src/Assets/Doctors.js
+++ src/Assets/Doctors.js
@@ -1,14 +1,24 @@
+import priyankimg from "./Images/PriyankqRawlani.png";
+
+
 const Doctors=[
 {
-    idphone:"6209220442",
+    idphone:"9702712979",
+    pronoun:"She/her",
     name:"Priyanka Rawlani",
     email:"priyankarawlani18@gmail.com",
     phone:"9702712979",
-    description:"Expressive arts therapy practitioner",
+    description:"Expressive Arts Therapy Practitioner",
     location:"Delhi",
-    experience:"4 Years",
+    experience:"1 Year",
     price:"1500 (with a sliding scale)",
-    pic:""
+    pic:priyankimg,
+    brief:`Hello! I’m Priyanka , an expressive arts therapy practitioner and an artist! 
+    I use the client centered approach in my therapy , where I guide the clients into taking the creative tools of expressive arts as a medium of expression and channelisation for their emotions. 
+    Expressing and relieving one’s feelings can lead to wellness and healing  - overall. And sometimes a lot of trauma, grief, emotions can get stuck in our bodies and our subconscious. 
+    The creative arts can become a guiding force for this relief and awareness! `,
+    specification:`I am Queer affirmative and Trauma informed in my training as well. 
+    I have also completed my training in Music in wellness from Berklee college of music. `
 }
 ]
-export default Doctors;
+export  default Doctors;
--- src/Component/ThrepySession/ThrepistList.js
+++ src/Component/ThrepySession/ThrepistList.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./Threpist-list.css";
 import ThrepistCard from "./ThrepistCard";
-
+import Doctors from "../../Assets/Doctors";
 const ThrepistList = () => {
   return (
-    <div className="container mt-5 mb-5 pt-5 text-center">
+    <div className="container-fluid mt-5 mb-5 pt-5 text-center">
     
       <div class="row justify-content-center align-items-center g-2">
-         <ThrepistCard/>
-         <ThrepistCard/>
-         <ThrepistCard/>
-         <ThrepistCard/>
+
+      {Doctors.map((val) => (
+  <ThrepistCard doctors={val}  key={val.idphone}/>
+))}
+
+
+        
+     
        
       </div>
     </div>
