c1ea4ed8e97c27a8d91e09a1ba9a8f6bf376382a
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-09-03 13:29:49 +0530

    commit

--- src/Assets/Doctors.js
+++ src/Assets/Doctors.js
@@ -6,7 +6,6 @@ import Asthaimg from "./Images/AsthaAnand.png";
 import Kratiimg from "./Images/Krati.png";
 import muskanimg from "./Images/aboutimg.jpeg";
 
-
 const Doctors = [
     {
         idphone: "7904730572",
@@ -14,16 +13,11 @@ const Doctors = [
         name: "Muskaan Jaiswal",
         email: "priyankarawlani18@gmail.com",
         phone: "9702712979",
-        description: `MSc. Clinical Psychology \n
-         \n PGD Expresive art therapy, Xaviers \n  Psychologist at the bariatric department, \n 
-        Apolo Hospital`,
-        
+        description: `MSc. Clinical Psychology \nPGD Expressive art therapy, Xaviers \nPsychologist at the bariatric department, \nApolo Hospital`,
         experience: "1.5 Years",
         price: "1500-2000 INR",
         pic: muskanimg,
-
-        brief: `My treatment focus is centered around my clients healing, exploration, and the illumination of their inner strengths.
-        Using evidence-based techniques and a compassionate heart, I work towards providing a safe space to help my clients navigate life's complexities and find clarity and resilience. I have an integrative therapy approach which involves tailoring the therapy to the individual client's needs and combining different therapeutic techniques. The goal is to provide a well-rounded and effective treatment that takes into account the unique characteristics of the client and their presenting concerns.`,
+        brief: `My treatment focus is centered around my clients' healing, exploration, and the illumination of their inner strengths. Using evidence-based techniques and a compassionate heart, I work towards providing a safe space to help my clients navigate life's complexities and find clarity and resilience. I have an integrative therapy approach, which involves tailoring the therapy to the individual client's needs and combining different therapeutic techniques. The goal is to provide a well-rounded and effective treatment that takes into account the unique characteristics of the client and their presenting concerns.`,
         specialisation: ``
     },
     {
@@ -32,22 +26,13 @@ const Doctors = [
         name: "Priyanka Rawlani",
         email: "priyankarawlani18@gmail.com",
         phone: "9702712979",
-        description: `MSc in Psychology (HRDM) - Christ University, Bangalore  \n
-    PG Diploma in Integrated Counselling - Nibbana accredited by Madras School of Social Work \n
-    Certification in Trauma Informed Care - TISS Mumbai `,
-    
+        description: `MSc in Psychology (HRDM) - Christ University, Bangalore\nPG Diploma in Integrated Counselling - Nibbana accredited by Madras School of Social Work\nCertification in Trauma Informed Care - TISS Mumbai`,
         experience: "1 Year",
         price: "1500 INR [Sliding scale available]",
         pic: priyankimg,
-
-        brief: `Hello! I’m Priyanka , an expressive arts therapy practitioner and an artist! 
-    I use the client centered approach in my therapy , where I guide the clients into taking the creative tools of expressive arts as a medium of expression and channelisation for their emotions. 
-    Expressing and relieving one’s feelings can lead to wellness and healing  - overall. And sometimes a lot of trauma, grief, emotions can get stuck in our bodies and our subconscious. 
-    The creative arts can become a guiding force for this relief and awareness! `,
-        specialisation: `I am Queer affirmative and Trauma informed in my training as well. 
-    I have also completed my training in Music in wellness from Berklee college of music. `
-    }
-    ,
+        brief: `Hello! I'm Priyanka, an expressive arts therapy practitioner and an artist! I use the client-centered approach in my therapy, where I guide the clients into taking the creative tools of expressive arts as a medium of expression and channelization for their emotions. Expressing and relieving one's feelings can lead to wellness and healing overall. Sometimes a lot of trauma, grief, and emotions can get stuck in our bodies and our subconscious. The creative arts can become a guiding force for this relief and awareness!`,
+        specialisation: `I am Queer affirmative and Trauma informed in my training as well. I have also completed my training in Music in wellness from Berklee college of music.`
+    },
     {
         idphone: "9702712979",
         pronoun: "She/her",
@@ -55,28 +40,25 @@ const Doctors = [
         email: "raushnikhanna@gmail.com",
         phone: "9702712979",
         description: "Expressive Arts Therapy Practitioner",
-        
         experience: "2.8 Years",
         price: "2000 - 4000 INR",
         addonData: "2000 for Indian Clients & 4000 for Indian clients living abroad or clients from other nationalities",
         pic: roshniimg,
-        brief: `I have provided professional assistance to clients spanning diverse geographical locations, addressing a wide spectrum of concerns. These encompass but are not limited to areas such as Relationship Dynamics, Occupational Stress, Strategic Career Counseling, Familial and Marital Challenges, Navigating Infidelity and Divorce, Enhancing Sexual Intimacy, Establishing Healthy Boundaries, Cultivating Self-Esteem, Overcoming Eating Disorders, Fostering Positive Body Image, Navigating Matters of Sexuality, and Proficient Financial Management. My expertise extends to encompass these realms and more, as I am committed to guiding clients towards holistic well-being and personal growth.I am passionate about creating content that encourages people to be vulnerable, self-reflective and self-accepting of themselves.`,
-        specialisation: `I mostly work with clients between the age of 18-40. years. I have helped clients across various geographies (India, Australia, Germany, UK, Canada, New Zealand & USA) with issues related to Relationships, Work related Stress, Career Guidance, Marital & Family issues, Infidelity, Divorce, Sexual Intimacy, Boundaries, Low Self-Esteem, Eating Disorders, Body Image, Sex & Sexuality, Financial Management, etc. I also conduct various mental health awareness sessions for Corporate Employees and College Students. Some of the organizations & institute that I have worked with include, Cisco, Sify, Social Beat, iLink Digital, etc. & M.O.P.Vaishnav College, Jai Hind College, Bharti College, DPS, etc. \n I am passionate about creating content that encourages people to be vulnerable, self-reflective and self-accepting of themselves through my Instagram page - @thesubtleartofexpression`
-    }
-    ,
+        brief: `I have provided professional assistance to clients spanning diverse geographical locations, addressing a wide spectrum of concerns. These encompass, but are not limited to, areas such as Relationship Dynamics, Occupational Stress, Strategic Career Counseling, Familial and Marital Challenges, Navigating Infidelity and Divorce, Enhancing Sexual Intimacy, Establishing Healthy Boundaries, Cultivating Self-Esteem, Overcoming Eating Disorders, Fostering Positive Body Image, Navigating Matters of Sexuality, and Proficient Financial Management. My expertise extends to encompass these realms and more, as I am committed to guiding clients toward holistic well-being and personal growth. I am passionate about creating content that encourages people to be vulnerable, self-reflective, and self-accepting of themselves.`,
+        specialisation: `I mostly work with clients between the age of 18-40. years. I have helped clients across various geographies (India, Australia, Germany, UK, Canada, New Zealand & USA) with issues related to Relationships, Work related Stress, Career Guidance, Marital & Family issues, Infidelity, Divorce, Sexual Intimacy, Boundaries, Low Self-Esteem, Eating Disorders, Body Image, Sex & Sexuality, Financial Management, etc. I also conduct various mental health awareness sessions for Corporate Employees and College Students. Some of the organizations & institute that I have worked with include, Cisco, Sify, Social Beat, iLink Digital, etc. & M.O.P.Vaishnav College, Jai Hind College, Bharti College, DPS, etc. \nI am passionate about creating content that encourages people to be vulnerable, self-reflective and self-accepting of themselves through my Instagram page - @thesubtleartofexpression`
+    },
     {
         idphone: "9702712979",
         pronoun: "She/her",
         name: "Ishani Ahuja",
         email: "ahujaishani@gmail.com",
         phone: "9702712979",
-        description: "Licensed Rehabilitation Counselor (RCI),Licensed Expressive Arts and Play Therapist",
-        
+        description: "Licensed Rehabilitation Counselor (RCI), Licensed Expressive Arts and Play Therapist",
         experience: "8 Years",
         price: " 2000-2800 INR [Sliding scale available]",
         pic: ishaniimg,
-        brief: `My name is ishani ahuja and I am an RCI licensed child , adolescent and young adult psychologist working with individuals , families from a spectrum of Neurotypical to Neurodivergent  beings. \n I am a trained and certified expressive arts based therapy practitioner,  play therapist and work in therapy  using  a trauma informed lens , engrained in mindfulness,  and eclectic  healing techniques for  making meaning of our own lives, through our own voices.  \n The nature of work involves all kinds of eclectic novel ways using arts, music, dance, poetry, movement, narratives, movie analysis, book /passage reviewing , metaphorical insights to make sense of our own world through our voice.`,
-        specialisation: `Play therapy both child and  adult, expressive arts based therapy, trauma informed therapy , inner child healing , emdr therapy, chakra healing , energy work healer , somatic healing, emotional freedom techniques `
+        brief: `My name is Ishani Ahuja, and I am an RCI licensed child, adolescent, and young adult psychologist working with individuals and families across the neurotypical to neurodivergent spectrum. I am a trained and certified expressive arts-based therapy practitioner and play therapist. I work in therapy using a trauma-informed lens, grounded in mindfulness and eclectic healing techniques to help individuals make meaning of their own lives through their own voices. The nature of my work involves employing various eclectic and novel approaches, including arts, music, dance, poetry, movement, narratives, movie analysis, book/passage review, and metaphorical insights to help individuals make sense of their own world through their unique voices.`,
+        specialisation: `Play therapy both child and  adult, expressive arts based therapy, trauma informed therapy , inner child healing , emdr therapy, chakra healing , energy work healer , somatic healing, emotional freedom techniques`
     },
     {
         idphone: "9113074429",
@@ -85,14 +67,11 @@ const Doctors = [
         email: " itsniramaya99@gmail.com",
         phone: "9113074429",
         description: "MSc Counselling psychology",
-  
         experience: "1 Year",
         price: " 1200-1500 INR",
         pic: Devanshiimg,
-        brief: `Hi, I am a counselling psychologist. I have around 450 hours of experience with counselling and deal with relationship issues, body image, self esteem, work related issues, trauma, sexual health, grief and loss and familial issues among others. I use a holistic approach including the mind, soul and the body. I incorporate a lot of somatic work with humanistic and existential theories and gestalt techniques.`,
-        specialisation: `
-
-        Gestalt therapy, Acceptance and commitment therapy        `
+        brief: `Hi, I am a counseling psychologist. I have approximately 450 hours of experience in counseling and address a range of issues, including relationship issues, body image, self-esteem, work-related concerns, trauma, sexual health, grief and loss, and familial issues, among others. I use a holistic approach that encompasses the mind, soul, and body. I incorporate a significant amount of somatic work, drawing from humanistic and existential theories, as well as gestalt techniques.`,
+        specialisation: `Gestalt therapy, Acceptance and commitment therapy`
     },
     {
         idphone: "9113074429",
@@ -100,19 +79,13 @@ const Doctors = [
         name: "Astha Anand",
         email: " itsniramaya99@gmail.com",
         phone: "9113074429",
-        description: "BA Psychology from DU. Msc in organisational psychology from Christ University. Post graduate diploma in counselling psychology from St Xaviers College.",
-       
+        description: "BA Psychology from DU. Msc in organisational psychology from Christ University. Postgraduate diploma in counselling psychology from St Xaviers College.",
         experience: "2 Years",
         price: " 1500 INR",
         pic: Asthaimg,
-        brief: ` 
-        I am an eclectic approach oriented therapist, I am queer affirmative and also open to cases that are in the bounds of a counselling psychologist like lifestyle issues, career, self esteem, teenage year issues, relationship issues, personal growth etc.         `,
-        specialisation: `
-        Eclectic approach. I am open to taking clients from the age group of 18-45 years. 
-
- `
-    }
-    ,
+        brief: `I am an eclectic approach-oriented therapist who is queer-affirmative and open to cases within the scope of a counseling psychologist. This includes addressing issues such as lifestyle concerns, career development, self-esteem, challenges faced during teenage years, relationship issues, and personal growth, among others.`,
+        specialisation: `Eclectic approach. I am open to taking clients from the age group of 18-45 years.`
+    },
     {
         idphone: "9113074429",
         pronoun: "She/her",
@@ -120,18 +93,12 @@ const Doctors = [
         email: "kratigirhotra10@gmail.com ",
         phone: "9113074429",
         description: "Expressive Arts Therapy Practitioner",
-        
         experience: "2 Years",
         price: " 1200-1500 INR",
         pic: Kratiimg,
-        brief: `I'm Krati Girhotra, and my expertise lies in the field of Expressive Arts Therapy. I've blended my intensive training in expressive arts and counseling with an intersectional, trauma-informed, queer-affirmative, and culturally relevant lens. Through a multimodal approach, I harnesses the transformative power of playfulness, empathy, and collaboration in my work with a wide spectrum of children & adults, unlocking immense potential for therapeutic growth.`
-        , specialisation: `
-        Children with special needs and their caretakers, group therapy 
- 
-
- `
+        brief: `I'm Krati Girhotra, and my expertise lies in the field of Expressive Arts Therapy. I've combined my intensive training in expressive arts and counseling with an intersectional, trauma-informed, queer-affirmative, and culturally relevant lens. Through a multimodal approach, I harness the transformative power of playfulness, empathy, and collaboration in my work with a wide spectrum of children and adults, unlocking immense potential for therapeutic growth.`,
+        specialisation: `Children with special needs and their caretakers, group therapy`
     }
+];
 
-
-]
 export default Doctors;
