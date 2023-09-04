f563c403e4b15afe18722e707988b39951c5c710
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-09-04 14:22:15 +0530

    change the primar

--- src/Component/WhyChooseUs/WyChooseUs.js
+++ src/Component/WhyChooseUs/WyChooseUs.js
@@ -3,20 +3,20 @@ import React from "react";
 const WyChooseUs = () => {
   return (
     <div className="Container text-center">
-      <span className="  shadow rounded p-1 brand">Why Choose Us</span>
+      <span className="display-4 why-choose-us slate">Why Choose Us</span>
      <div className="mx-auto w-70">
         
       <p className="mt-5 text-start">
-        <span className="bold fs-5">Customized Solutions:</span> We understand that every organization is unique.
+        <span className="bold fs-4">Customized Solutions:</span> We understand that every organization is unique.
         Our workshops are tailored to your specific needs, ensuring that your
         team gains relevant skills and knowledge.
      <br/>
-     <span className="bold fs-5"> Expert Guidance:</span>  With a deep understanding of psychology and workplace
+     <span className="bold fs-4"> Expert Guidance:</span>  With a deep understanding of psychology and workplace
         dynamics, we are committed to delivering workshops that are insightful,
         engaging, and transformative.
     <br/>
-      <span  className="bold fs-5">  Lasting Impact:</span> Our workshops are not just events - they are experiences
-        that inspire lasting positive change. Book Your Workshop
+      <span  className="bold fs-4">  Lasting Impact:</span> Our workshops are not just events - they are experiences
+        that inspire lasting positive change.
       <br/>
         Invest in the well-being and success of your team today. Book a
         transformative workshop with us and take the first step towards a
