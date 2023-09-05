b7b369080ad3d318afe2de620fdbc2ef8a53170a
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-09-05 23:30:45 +0530

    Commit the changes

--- src/Component/Reviews/Reviews.js
+++ src/Component/Reviews/Reviews.js
@@ -1,76 +1,86 @@
 import React from "react";
 import "./slider.css";
-import {GrFormPrevious} from "react-icons/gr"
-import {GrFormNext} from "react-icons/gr"
-import {RiDoubleQuotesL} from "react-icons/ri"
-import {RiDoubleQuotesR} from "react-icons/ri"
+import { GrFormPrevious } from "react-icons/gr"
+import { GrFormNext } from "react-icons/gr"
+import { RiDoubleQuotesL } from "react-icons/ri"
+import { RiDoubleQuotesR } from "react-icons/ri"
+import { AiFillStar } from "react-icons/ai"
+import { FaUserCircle } from "react-icons/fa"
+
 const Reviews = () => {
-  return (
-    <div className="slider container">
-      <div className="slider">
-        <div
-          id="carouselExampleControlsNoTouching"
-          class="carousel slide"
-          data-bs-touch="false"
-          data-bs-interval="false"
-        >
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-            <div className='reviewCard'>
-          
-   <div className="card-text">
-    <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)"/> A responsive and interactive Todo List app with drag-and-drop functionality and CRUD operations. This app is built using React, Bootstrap, React Bootstrap, and Material-UI.
-    <RiDoubleQuotesR fontSize={"2rem"}  color=" rgb(255,82,161)"/>
-    </span>
+    return (
+        <div className="slider container">
+            <div className="slider">
+                <div
+                    id="carouselExampleControlsNoTouching"
+                    class="carousel slide"
+                    data-bs-touch="false"
+                    data-bs-interval="false"
+                >
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div className='reviewCard'>
 
-   </div>
-   <div>
-    <button>Slignup</button>
-   </div>
-         
-    </div>
+                                <div className="card-text">
+                                    <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                        I can't speak highly enough about the Clinical Psychology program. The mentors are not just knowledgeable but also incredibly supportive. The curriculum is well-structured, and the practical experience I gained during the program was invaluable. I now feel confident in my ability to go forward in the field. This program has opened doors to a fulfilling career.    <RiDoubleQuotesR fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                    </span>
+
+                                </div>
+                                <div className="review-user">
+                                    <span><FaUserCircle fontSize={"3rem"} color="rgb(255,82,161)"/></span>
+                                    <div className="ms-2  text-start">
+                                        <span className="stars">
+                                            <AiFillStar color="rgb(255,82,161)" />
+                                            <AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /></span>
+                                        <span className="name d-block">Nishant Kumar [Student]</span>
+                                       
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div className='reviewCard'>
+
+
+                                <span> A responsive and interactive Todo List app with drag-and-drop functionality and CRUD operations. This app is built using React, Bootstrap, React Bootstrap, and Material-UI.
+
+                                </span>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div className='reviewCard'>
+
+
+                                <span> A responsive and interactive Todo List app with drag-and-drop functionality and CRUD operations. This app is built using React, Bootstrap, React Bootstrap, and Material-UI.
+
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <button
+                        class="carousel-control-prev text-dark"
+                        type="button"
+                        data-bs-target="#carouselExampleControlsNoTouching"
+                        data-bs-slide="prev"
+                    >
+
+                        <span className="fs-1"><GrFormPrevious /></span>
+                    </button>
+                    <button
+                        class="carousel-control-next  text-dark"
+                        type="button"
+                        data-bs-target="#carouselExampleControlsNoTouching"
+                        data-bs-slide="next"
+                    >
+
+                        <span className="fs-1"><GrFormNext /></span>
+                    </button>
+                </div>
             </div>
-            <div class="carousel-item">
-            <div className='reviewCard'>
-          
-
-          <span> A responsive and interactive Todo List app with drag-and-drop functionality and CRUD operations. This app is built using React, Bootstrap, React Bootstrap, and Material-UI.
-
-</span>
-    </div>
-            </div>
-            <div class="carousel-item">
-            <div className='reviewCard'>
-          
-
-          <span> A responsive and interactive Todo List app with drag-and-drop functionality and CRUD operations. This app is built using React, Bootstrap, React Bootstrap, and Material-UI.
-
-</span>
-    </div>
-            </div>
-          </div>
-          <button
-            class="carousel-control-prev text-dark"
-            type="button"
-            data-bs-target="#carouselExampleControlsNoTouching"
-            data-bs-slide="prev"
-          >
-           
-           <span className="fs-1"><GrFormPrevious/></span>
-          </button>
-          <button
-            class="carousel-control-next  text-dark"
-            type="button"
-            data-bs-target="#carouselExampleControlsNoTouching"
-            data-bs-slide="next"
-          >
-           
-            <span className="fs-1"><GrFormNext/></span>
-          </button>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Reviews;
