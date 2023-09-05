c35c20412416f52c7771485af782151aaee1686a
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-09-06 00:40:24 +0530

    change it

--- src/Component/Reviews/Reviews.js
+++ src/Component/Reviews/Reviews.js
@@ -18,16 +18,15 @@ const Reviews = () => {
                     data-bs-interval="false"
                 >
                     <div class="carousel-inner">
+                     
                         <div class="carousel-item active">
                             <div className='reviewCard'>
 
-                                <div className="card-text">
-                                    <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)" />
+
+                            <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)" />
                                         I can't speak highly enough about the Clinical Psychology program. The mentors are not just knowledgeable but also incredibly supportive. The curriculum is well-structured, and the practical experience I gained during the program was invaluable. I now feel confident in my ability to go forward in the field. This program has opened doors to a fulfilling career.    <RiDoubleQuotesR fontSize={"2rem"} color=" rgb(255,82,161)" />
                                     </span>
-
-                                </div>
-                                <div className="review-user">
+                                    <div className="review-user">
                                     <span><FaUserCircle fontSize={"3rem"} color="rgb(255,82,161)"/></span>
                                     <div className="ms-2  text-start">
                                         <span className="stars">
@@ -37,27 +36,85 @@ const Reviews = () => {
                                        
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item ">
                             <div className='reviewCard'>
 
 
-                                <span> A responsive and interactive Todo List app with drag-and-drop functionality and CRUD operations. This app is built using React, Bootstrap, React Bootstrap, and Material-UI.
-
-                                </span>
+                            <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                        I can't speak highly enough about the Clinical Psychology program. The mentors are not just knowledgeable but also incredibly supportive. The curriculum is well-structured, and the practical experience I gained during the program was invaluable. I now feel confident in my ability to go forward in the field. This program has opened doors to a fulfilling career.    <RiDoubleQuotesR fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                    </span>
+                                    <div className="review-user">
+                                    <span><FaUserCircle fontSize={"3rem"} color="rgb(255,82,161)"/></span>
+                                    <div className="ms-2  text-start">
+                                        <span className="stars">
+                                            <AiFillStar color="rgb(255,82,161)" />
+                                            <AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /></span>
+                                        <span className="name d-block">Nishant Kumar [Student]</span>
+                                       
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item ">
                             <div className='reviewCard'>
 
 
-                                <span> A responsive and interactive Todo List app with drag-and-drop functionality and CRUD operations. This app is built using React, Bootstrap, React Bootstrap, and Material-UI.
-
-                                </span>
+                            <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                        I can't speak highly enough about the Clinical Psychology program. The mentors are not just knowledgeable but also incredibly supportive. The curriculum is well-structured, and the practical experience I gained during the program was invaluable. I now feel confident in my ability to go forward in the field. This program has opened doors to a fulfilling career.    <RiDoubleQuotesR fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                    </span>
+                                    <div className="review-user">
+                                    <span><FaUserCircle fontSize={"3rem"} color="rgb(255,82,161)"/></span>
+                                    <div className="ms-2  text-start">
+                                        <span className="stars">
+                                            <AiFillStar color="rgb(255,82,161)" />
+                                            <AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /></span>
+                                        <span className="name d-block">Nishant Kumar [Student]</span>
+                                       
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                        <div class="carousel-item ">
+                            <div className='reviewCard'>
+
+
+                            <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                        I can't speak highly enough about the Clinical Psychology program. The mentors are not just knowledgeable but also incredibly supportive. The curriculum is well-structured, and the practical experience I gained during the program was invaluable. I now feel confident in my ability to go forward in the field. This program has opened doors to a fulfilling career.    <RiDoubleQuotesR fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                    </span>
+                                    <div className="review-user">
+                                    <span><FaUserCircle fontSize={"3rem"} color="rgb(255,82,161)"/></span>
+                                    <div className="ms-2  text-start">
+                                        <span className="stars">
+                                            <AiFillStar color="rgb(255,82,161)" />
+                                            <AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /></span>
+                                        <span className="name d-block">Nishant Kumar [Student]</span>
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item ">
+                            <div className='reviewCard'>
+
+
+                            <span> <RiDoubleQuotesL fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                        I can't speak highly enough about the Clinical Psychology program. The mentors are not just knowledgeable but also incredibly supportive. The curriculum is well-structured, and the practical experience I gained during the program was invaluable. I now feel confident in my ability to go forward in the field. This program has opened doors to a fulfilling career.    <RiDoubleQuotesR fontSize={"2rem"} color=" rgb(255,82,161)" />
+                                    </span>
+                                    <div className="review-user">
+                                    <span><FaUserCircle fontSize={"3rem"} color="rgb(255,82,161)"/></span>
+                                    <div className="ms-2  text-start">
+                                        <span className="stars">
+                                            <AiFillStar color="rgb(255,82,161)" />
+                                            <AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /><AiFillStar color="rgb(255,82,161)" /></span>
+                                        <span className="name d-block">Nishant Kumar [Student]</span>
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
                     </div>
                     <button
                         class="carousel-control-prev text-dark"
